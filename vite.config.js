26689489349e1b593b072c20f52f80674951a536
empresadeexito18-dot <empresadeexito18@gmail.com>
2026-05-11 19:30:57 -0500

    Configuración de despliegue lista

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // REEMPLAZA 'nombre-del-repo' por el nombre exacto que le pusiste en GitHub
+  base: '/nombre-del-repo/', 
 })
